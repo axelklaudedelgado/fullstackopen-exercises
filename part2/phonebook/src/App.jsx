55b465a9bf9fef4b0bc9eaae9563af42cc25d351
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2024-11-04 12:51:35 +0800

    feat: add part2 phonebook 2.11 solution

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const Filter = ({value, onFilterChange}) => {
   return(
@@ -42,16 +43,19 @@ const Person = ({key, name, number}) => <p key={key}>{name} {number}</p>
 const Persons = ({personList}) => <div>{personList.map(person => <Person key={person.id} name={person.name} number={person.number}/>)}</div>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]); 
+  const [persons, setPersons] = useState([]); 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterText, setFilterText] = useState('');
   const [filteredPersons, setFilteredPersons] = useState(persons);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault();
